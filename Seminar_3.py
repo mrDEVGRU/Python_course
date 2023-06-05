50d0130249d6e52405fac2b54677af35a93e95b0
mrDEVGRU <mrdevgru@gmail.com>
2023-06-05 17:20:52 +0400

    ex18

--- Seminar_3.py
+++ Seminar_3.py
@@ -7,12 +7,31 @@
 #     3
 #     -> 1
 
+# n = int(input("Введите размер массива: "))
+# array = list(range(1, n + 1))
+# print(array)
+# found = int(input("Введите число: "))
+# count = 0
+# for i in range(len(array)):
+#     if array[i] == found:
+#         count += 1
+# print(count)
+
+
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X
+# *Пример:* 5 [1 2 3 4 5] 6 -> 5
+
 n = int(input("Введите размер массива: "))
 array = list(range(1, n + 1))
 print(array)
 found = int(input("Введите число: "))
-count = 0
-for i in range(len(array)):
-    if array[i] == found:
-        count += 1
-print(count)
+min = abs(found - array[0])
+index = 0
+for i in range(1, n):
+    count = abs(found - array[i])
+    if count < min:
+        min = count
+        index = i
+print(array[index])
