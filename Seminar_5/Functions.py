3de05344767e0170831a8727d4c387c6abf0d2af
mrDEVGRU <mrdevgru@gmail.com>
2023-06-10 16:37:08 +0400

    Ex_28

--- Seminar_5/Functions.py
+++ Seminar_5/Functions.py
@@ -2,3 +2,10 @@ def degree(a, b):
     if b == 0:
         return 1
     return a * degree(a, b - 1)
+
+
+def recurs_sum(a, b):
+    if b == 0:
+        return a
+    else:
+        return recurs_sum(a + 1, b - 1)
