709ff1e7bd15117eafcaa7d5b7ae7fe4db30bb92
mrDEVGRU <mrdevgru@gmail.com>
2023-06-21 18:47:37 +0400

    Ex_36

--- Seminar_7/Ex_36.py
+++ Seminar_7/Ex_36.py
@@ -4,11 +4,11 @@
 
 
 # def print_operation_table(operation, num_rows=6, num_columns=6):
-#     arr = [
+#     array = [
 #         [operation(i, j) for i in range(1, num_rows + 1)]
 #         for j in range(1, num_columns + 1)
 #     ]
-#     for i in arr:
+#     for i in array:
 #         print(*(f"{x:>2}" for x in i))
 
 
